3247bb9c326f59e9996f816aeadb29b5c1ecb45d
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2025-11-02 18:29:49 +0100

    feat: sprawdzanie nowych anomalii + wnioski

--- sql/02_wstepna_analiza/identyfikacja_anomalii.sql
+++ sql/02_wstepna_analiza/identyfikacja_anomalii.sql
@@ -7,14 +7,33 @@ SELECT
 	o.order_id,
 	oi.order_id,
 	oi.order_item_id,
-	oi.price,
-	oi.freight_value
+	o.order_delivered_customer_date,
+	o.order_delivered_carrier_date
 FROM
 	orders o
 INNER JOIN
 	order_items oi ON o.order_id = oi.order_id
 WHERE
 	order_delivered_customer_date < order_delivered_carrier_date;
+-- Problem polega na tym że data dostarczenia do klienta jest wcześneijsza niż data dostarczenia przez kuriera.
+
+SELECT
+	order_id
+FROM
+	order_items
+WHERE
+	price <= 0;
+
+-- OPINIE
+SELECT
+	review_id,
+	review_creation_date,
+	review_answer_timestamp
+FROM
+	order_reviews
+WHERE
+	review_creation_date > review_answer_timestamp::date;
+
 
 
 
