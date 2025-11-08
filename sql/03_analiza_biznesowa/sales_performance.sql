f55419c67f04a3610eaf7a3ca06806aea0d09b07
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2025-11-08 18:36:46 +0100

    feat: 4 additional analysis
    Top kategorie produktów według sumy sprzedaży,
    Sezonowość sumy zamówień według miesięcy, dnia tygodnia, godziny

--- sql/03_analiza_biznesowa/sales_performance.sql
+++ sql/03_analiza_biznesowa/sales_performance.sql
@@ -42,3 +42,99 @@ GROUP BY
 ORDER BY
 	sales DESC;
 
+
+-- Top kategorie produktów według sumy sprzedaży
+SELECT
+	cn.product_category_name_english,
+	ROUND(SUM(oi.price+oi.freight_value)::numeric,2) as sales
+FROM
+	order_items oi
+INNER JOIN
+	products p ON oi.product_id = p.product_id
+LEFT JOIN
+	category_name cn ON p.product_category_name = cn.product_category_name
+GROUP BY
+	cn.product_category_name_english
+ORDER BY
+	sales DESC;
+
+
+-- Sezonowość sumy zamówień według miesięcy
+SELECT
+	ROUND(SUM(oi.price + oi.freight_value)) as sales,
+	EXTRACT(MONTH FROM o.order_purchase_timestamp) as month
+FROM
+	orders o
+INNER JOIN
+	order_items oi ON o.order_id = oi.order_id
+GROUP BY
+	month
+ORDER BY
+	month;
+
+-- Sezonowość sumy zamówień według dnia tygodnia
+SELECT
+	TO_CHAR(o.order_purchase_timestamp, 'day') as day_of_week,
+	ROUND(SUM(oi.price + oi.freight_value)::numeric, 2) as sales
+FROM
+	orders o
+INNER JOIN
+	order_items oi ON o.order_id = oi.order_id
+GROUP BY
+	day_of_week
+ORDER BY
+	sales DESC;
+
+-- Sezonowość sumy zamówień według godziny
+SELECT
+	ROUND(SUM(oi.price + oi.freight_value)) as sales,
+	EXTRACT(HOUR FROM o.order_purchase_timestamp) as hour
+FROM
+	orders o
+INNER JOIN
+	order_items oi ON o.order_id = oi.order_id
+GROUP BY
+	hour
+ORDER BY
+	sales DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
