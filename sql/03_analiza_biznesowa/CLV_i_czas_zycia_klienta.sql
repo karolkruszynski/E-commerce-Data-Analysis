945a2733a413f80207e767f0f148beb95589b2a2
Karol Kruszyński <90861840+karolkruszynski@users.noreply.github.com>
2025-11-11 12:38:23 +0100

    feat: extended customer life cycle analysis
    by state

--- sql/03_analiza_biznesowa/CLV_i_czas_zycia_klienta.sql
+++ sql/03_analiza_biznesowa/CLV_i_czas_zycia_klienta.sql
@@ -76,6 +76,10 @@ ORDER BY
 -- Czas życia klienta mierzony od pierwszego do ostatniego zakupu.
 SELECT
 	c.customer_unique_id,
+	CASE
+		WHEN c.customer_state IN ('RJ','SP','MG') THEN 1
+		ELSE 0
+	END as is_client_from_top3_state,
 	EXTRACT(DAY FROM MAX(o.order_purchase_timestamp) - MIN(o.order_purchase_timestamp)) as client_lifetime_days,
 	COUNT(*) as orders_count
 FROM
@@ -83,15 +87,41 @@ FROM
 INNER JOIN
 	customers c ON o.customer_id = c.customer_id
 GROUP BY
-	c.customer_unique_id
+	c.customer_unique_id, c.customer_state
 ORDER BY
 	client_lifetime_days DESC, 
 	orders_count;
 
 
-
-
-
+-- Czas życia klienta mierzony od pierwszego do ostatniego zakupu według stanów
+WITH top3_state as 
+(
+SELECT
+	c.customer_unique_id,
+	CASE
+		WHEN c.customer_state IN ('RJ','SP','MG') THEN 1
+		ELSE 0
+	END as is_client_from_top3_state,
+	EXTRACT(DAY FROM MAX(o.order_purchase_timestamp) - MIN(o.order_purchase_timestamp)) as client_lifetime_days,
+	COUNT(*) as orders_count
+FROM
+	orders o
+INNER JOIN
+	customers c ON o.customer_id = c.customer_id
+GROUP BY
+	c.customer_unique_id, c.customer_state
+ORDER BY
+	client_lifetime_days DESC, 
+	orders_count
+LIMIT 100
+)
+SELECT
+	t3s.is_client_from_top3_state,
+	COUNT(t3s.is_client_from_top3_state)
+FROM
+	top3_state t3s
+GROUP BY
+	t3s.is_client_from_top3_state
 
 
 
